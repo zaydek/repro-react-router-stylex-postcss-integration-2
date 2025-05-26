1b5d2f4770e3c843b0c129991ca8a50a1f720027
Zaydek MG <zaydekdotcom@gmail.com>
2025-05-25 20:35:14 -0700

    Consistent comment

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -3,9 +3,9 @@ module.exports = {
   plugins: {
     "@stylexjs/postcss-plugin": {
       include: [
-        // Ensure StyleX files (e.g. .ts, .tsx) are preprocessed
+        // Ensure StyleX files (e.g. *.ts, *.tsx) are preprocessed
         "app/**/*.{js,jsx,ts,tsx}",
-        // Ensure StyleX static files (e.g. .stylex.js, .stylex.ts) are preprocessed
+        // Ensure StyleX static files (e.g. *.stylex.ts) are preprocessed
         "app/**/*.{stylex.js,stylex.ts}",
       ],
       useCSSLayers: true, // Optional
